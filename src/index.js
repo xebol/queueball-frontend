8a8333b9608c75dcd0d489938e7be17421a35eb8
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-11-28 12:37:30 -0700

    deploy project on netlify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "normalize.css";
 import App from "./App";
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
